1b3da7aed59a9c6aa095fb0f33b123314ddc63f7
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2016-01-14 17:41:38 +1100

    Prototyping main page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import RaisedButton from 'material-ui/lib/raised-button';
 import Checkbox from 'material-ui/lib/checkbox';
+import AppBar from 'material-ui/lib/app-bar';
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import ContentInbox from 'material-ui/lib/svg-icons/content/inbox';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello, world.</h1>
-        <RaisedButton label="Default" />
-        <Checkbox />
+        <AppBar
+          title="Plan"
+          iconClassNameRight="muidocs-icon-navigation-expand-more"
+        />
+        <List>
+          <ListItem primaryText="Matthew" leftIcon={<ContentInbox />} />
+          <ListItem primaryText="Mark" leftIcon={<ContentInbox />} />
+          <ListItem primaryText="John" leftIcon={<ContentInbox />} />
+          <ListItem primaryText="Luke" leftIcon={<ContentInbox />} />
+        </List>
+        <RaisedButton label="Save" />
       </div>
     );
   }
